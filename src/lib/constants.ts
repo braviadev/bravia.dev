e553d6b8cb4f07614d1e2b3f354f397ad232a6c4
Olanrewaju Toyyib Olaitan <olarewajutoyyib@gmail.com>
2026-02-27 17:34:18 +0100

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,15 +5,15 @@ export const IS_PRODUCTION = env.NODE_ENV === 'production'
 // eslint-disable-next-line unicorn/no-typeof-undefined -- window is not always defined
 export const IS_SERVER = typeof globalThis.window === 'undefined'
 
-export const GITHUB_USERNAME = 'nelsonlaidev'
+export const GITHUB_USERNAME = 'braviadev'
 
-export const MY_NAME = 'Nelson Lai'
+export const MY_NAME = 'Olanrewaju Toyyib'
 
-export const SITE_GITHUB_URL = 'https://github.com/nelsonlaidev'
-export const SITE_FACEBOOK_URL = 'https://www.facebook.com/nelsonlaidev'
-export const SITE_INSTAGRAM_URL = 'https://www.instagram.com/nelsonlaidev'
-export const SITE_X_URL = 'https://x.com/nelsonlaidev'
-export const SITE_YOUTUBE_URL = 'https://www.youtube.com/@nelsonlaidev'
+export const SITE_GITHUB_URL = 'https://github.com/braviadev'
+export const SITE_FACEBOOK_URL = 'https://www.facebook.com/olanrewajutoyyiib'
+export const SITE_INSTAGRAM_URL = 'https://www.instagram.com/olanrewajutoyyiib'
+export const SITE_X_URL = 'https://x.com/O_Toyyib'
+export const SITE_YOUTUBE_URL = 'https://youtube.com/@olanrewajutoyyib'
 
 export const OG_IMAGE_WIDTH = 1200
 export const OG_IMAGE_HEIGHT = 630
