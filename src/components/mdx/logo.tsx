5cee6b77bc96b878dd3b0736ac776b37ae9ade03
Olanrewaju Toyyib Olaitan <olarewajutoyyib@gmail.com>
2026-03-08 15:27:53 +0100

    Update logo.tsx

--- src/components/mdx/logo.tsx
+++ src/components/mdx/logo.tsx
@@ -1,16 +1,17 @@
-import { Logo as BraviaLogo } from '@/components/ui/logo'
+// Example preview component using the logo
+import { Logo as BraviaLogo } from '@/components/ui/logo';
 
 function Logo() {
   return (
-    <div className='flex flex-col gap-4 md:flex-row'>
-      <div className='flex h-52 w-full items-center justify-center rounded-lg bg-white'>
-        <BraviaLogo className='text-black' width={48} />
+    <div className="flex flex-col gap-4 md:flex-row">
+      <div className="flex h-52 w-full items-center justify-center rounded-lg bg-white">
+        <BraviaLogo className="text-black" width={48} />
       </div>
-      <div className='flex h-52 w-full items-center justify-center rounded-lg bg-black'>
-        <BraviaLogo className='text-white' width={48} />
+      <div className="flex h-52 w-full items-center justify-center rounded-lg bg-black">
+        <BraviaLogo className="text-white" width={48} />
       </div>
     </div>
-  )
+  );
 }
 
-export default Logo
+export default Logo;
