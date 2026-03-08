e253175a75391a1f3f460ff098d0191ce21cf6aa
Olanrewaju Toyyib Olaitan <olarewajutoyyib@gmail.com>
2026-03-08 16:57:20 +0100

    Update logo.tsx

--- src/components/mdx/logo.tsx
+++ src/components/mdx/logo.tsx
@@ -5,10 +5,10 @@ function Logo() {
   return (
     <div className="flex flex-col gap-4 md:flex-row">
       <div className="flex h-150 w-full items-center justify-center rounded-lg bg-white">
-        <BraviaLogo className="text-black" width={160} />
+        <BraviaLogo className="text-black" width={48} />
       </div>
       <div className="flex h-150 w-full items-center justify-center rounded-lg bg-black">
-        <BraviaLogo className="text-white" width={160} />
+        <BraviaLogo className="text-white" width={48} />
       </div>
     </div>
   );
